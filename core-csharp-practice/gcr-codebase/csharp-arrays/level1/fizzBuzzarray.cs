ca61e943e4004c1fd243577fa55beaad510b09df
Nkrajput12 <nand.kishor_cs22@gla.ac.in>
2025-12-23 18:34:35 +0530

    [Nand kishor] Add: add 10 problems in level 1 of arrays

---
+++ core-csharp-practice/gcr-codebase/csharp-arrays/level1/fizzBuzzarray.cs
@@ -0,0 +1,47 @@
+﻿using System;
+class fizzBuzzarray
+{
+    static void Main(string[] args)
+    {
+        //taking input
+        Console.WriteLine("Enter a number");
+        int n = Convert.ToInt32(Console.ReadLine());
+        
+        if(n<= 0)
+        {
+            Console.Error.WriteLine("please enter a natural number");
+            Environment.Exit(0);
+        }
+
+        string[] str = new string[n];
+
+        //store in array
+        for (int i = 1; i <= n; i++)
+        {
+            if (i % 3 == 0 && i % 5 == 0)
+            {
+                str[i - 1] = "FizzBuzz";
+            }
+            else if (i % 3 == 0)
+            {
+                str[i - 1] = "Fizz";
+            }
+            else if (i % 5 == 0)
+            {
+                str[i - 1] = "Buzz";
+            }
+            else
+            {
+                str[i - 1] = i.ToString();
+            }
+        }
+        // Print array
+        for(int i = 0; i < n; i++)
+        {
+            Console.WriteLine(str[i]);
+        }
+        
+    }
+
+}
+
