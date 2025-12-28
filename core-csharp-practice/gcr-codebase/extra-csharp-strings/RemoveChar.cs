9400b5479b6c22179a5c7e620c74f74cb33c5499
Nkrajput12 <nand.kishor_cs22@gla.ac.in>
2025-12-28 13:16:57 +0530

    [Nand kishor] Add: add 12 solve problems in extra-csharp-strings

---
+++ core-csharp-practice/gcr-codebase/extra-csharp-strings/RemoveChar.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+public class RemoveChar
+{
+    //method for removing the character from string
+    public static string Remove(string str , char c)
+    {
+        string result = "";
+        
+        foreach (char ch in str)
+        {
+            if (ch != c)
+            {
+                result += ch;
+            }
+        }
+
+        return result;
+    }
+
+    public static void Main(string[] args)
+    {
+        //taking input 
+        Console.Write("Enter the text: ");
+        string str = Console.ReadLine() ?? "";
+        Console.Write("Enter the character you want to remove: ");
+        char c = Convert.ToChar(Console.ReadLine());
+
+        //call method
+        string result = Remove(str , c);
+
+        Console.WriteLine("result = " + result);
+    }
+}
