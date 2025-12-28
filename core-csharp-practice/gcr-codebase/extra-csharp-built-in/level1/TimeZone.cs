69ecf1ae515e653e49f6cb5355522b7802bbc073
Nkrajput12 <nand.kishor_cs22@gla.ac.in>
2025-12-28 20:59:22 +0530

    [Nand kishor] Add: add13 soved problems in extra-csharp-built-in

---
+++ core-csharp-practice/gcr-codebase/extra-csharp-built-in/level1/TimeZone.cs
@@ -0,0 +1,20 @@
+﻿using System;
+class TimeZone
+{
+    //method to display current time in time zone
+    public static  void ShowTimeZone()
+    {
+        DateTimeOffset gmt = DateTimeOffset.UtcNow;
+        TimeZoneInfo ist = TimeZoneInfo.FindSystemTimeZoneById("India Standard Time");
+        TimeZoneInfo pst = TimeZoneInfo.FindSystemTimeZoneById("Pacific Standard Time");
+        //printing time in gmt,ist & pst
+        Console.WriteLine("GMT TimeZone: " + gmt);
+        Console.WriteLine("IST TimeZone: " + TimeZoneInfo.ConvertTime(gmt, ist));
+        Console.WriteLine("PST TimeZone: " + TimeZoneInfo.ConvertTime(gmt, pst));
+    }
+
+    public static void Main(string[] args)
+    {
+        ShowTimeZone();
+    }
+}
---
+++ core-csharp-practice/gcr-codebase/extra-csharp-built-in/level2/PalindromeString.cs
@@ -0,0 +1,36 @@
+﻿using System;
+class PalindromeString
+{
+    //checking for palindrome
+    public static bool IsPalindrome(string str)
+    {
+        char[] rev1 = str.ToCharArray();
+        Array.Reverse(rev1);
+        string rev = new String(rev1);
+
+        if (rev.Equals(str)) return true;
+        else return false;
+
+    }
+
+    //method for display the result
+    public static void Display(bool b)
+    {
+        if (b) Console.WriteLine("Palindrome string");
+        else Console.WriteLine("Not a Palindrome string");
+    }
+
+    //main method 
+    public static void Main(string[] args)
+    {
+        //taking input 
+        Console.WriteLine("enter the string:");
+        string str = Console.ReadLine() ?? "";
+
+        bool result = IsPalindrome(str);
+
+        Display(result);
+
+    }
+}
+  
