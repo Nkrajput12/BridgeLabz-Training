2862b1d9ce1143c62ae9a1b4d7b93e65b07e6a51
Nkrajput12 <nand.kishor_cs22@gla.ac.in>
2025-12-29 16:51:10 +0530

    [Nand kishor] Add: add the two solve scenerio based problems

---
+++ core-csharp-practice/scenario-based/SentenceFormatter.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Text;
+class SentenceFormatter
+{
+    //mehod for add one space after punctuation 
+    public static string AddSpace(string text)
+    {
+
+        string sb = ""; //intiallizing the empty string 
+        for (int i = 0; i < text.Length-1; i++)
+        {
+            char c = text[i];
+            
+             if (char.IsPunctuation(c)) // check for the punctuation
+             {
+                 sb += c;
+                 sb += " "; //if there is a punctuation then add a space after it
+             }
+             else
+             {
+                sb += c;
+             }
+        }
+        sb += text[text.Length - 1]; //add last character to the string because last character is mainly punctuation and we don't want to add space in front of it
+        return sb;
+            
+    }
+
+    //method for trim the spaces
+    public static string TrimSpace(string text)
+    {
+        string sb = "";
+        for(int i = 0;i< text.Length ; i++)
+        {
+            char c = text[i];
+  
+            if(!(c== ' ' && text[i+1]==' ')) //check for space after the space if this is not true then add element
+            {
+                sb += c;
+            }
+        }
+        return sb;
+    }
+
+    //mehod to captilize the letter after period/question mark and exclamation mark
+    public static string Capital(string text)
+    {
+        string sb = "";
+        sb += char.ToUpper(text[0]); //capitialize the first letter
+        if (text.Length == 1) return sb; //if the text length is equal to 1 
+        sb += text[1]; // add the second letter to sb
+        for(int i=2;i< text.Length; i++)
+        {
+            char c = text[i-2];
+            if(c == '!' || c== '?' || c == '.') //check for the !/?/. 2 char before
+            {
+                sb += char.ToUpper(text[i]); // if the above condition is true capitalize the letter
+            }
+            else
+            {
+                sb += text[i]; 
+            }
+        }
+        
+        return sb;
+    }
+
+    //main method 
+    public static void Main(string[] args)
+    {
+        //taking input
+        Console.WriteLine("Enter the text");
+        string text = Console.ReadLine() ?? "";
+
+        //call dispay method
+        Display(text);
+        
+    }
+
+    //method for display
+    public static void Display(string text)    
+    {
+        string space = AddSpace(text); // call method to add spaces in front of punctuation
+        string trim = TrimSpace(space); //call method to trim the extra spaces
+        string capital = Capital(trim); //call method to captilize the first letter after the ./!/?.
+        Console.WriteLine(capital);
+    }
+
+}
--- core-csharp-practice/scenario-based/SnakeNLadder.cs
+++ core-csharp-practice/scenario-based/SnakeNLadder.cs
@@ -5,18 +5,18 @@ class SnakeNLadder
     //method to generate a random number 1-6
     public static int RandomDice()
     {
-        Random rand = new Random();
-        return rand.Next(1,7);
+        Random rand = new Random(); // declare object of Random Class
+        return rand.Next(1,7); // return random number between 1 to 6
     }
     //mehtod for checking snake
     public static int[] CheckSnake(int[,] snake,  int[] score, int chance)
     {
         for(int i = 0; i < snake.GetLength(0); i++)
         {
-            if (score[chance] == snake[i , 0])
+            if (score[chance] == snake[i , 0]) //check if our score is equal to 1column of snake 
             {
                 Console.WriteLine("Oh no! bitten by snake at " + snake[i, 0] + " and down to " + snake[i, 1] + " !");
-                score[chance] = snake[i, 1];
+                score[chance] = snake[i, 1]; //down the score of player to respected 2 column of the snake 
             }
         }
 
@@ -28,10 +28,10 @@ class SnakeNLadder
     {
         for (int i = 0; i < ladder.GetLength(0); i++)
         {
-            if (score[chance] == ladder[i, 0])
+            if (score[chance] == ladder[i, 0]) //check if our score is equal to 1column of ladder
             {
                 Console.WriteLine("Yay! climb a ladder at " + ladder[i, 0] + " and up to " + ladder[i, 1]);
-                score[chance] = ladder[i, 1];
+                score[chance] = ladder[i, 1]; // if yes climb the ladder
             }
         }
 
@@ -41,15 +41,15 @@ class SnakeNLadder
     //mehod for moving
     public static int[] Move(int chance,int roll, int[] score)
     {
-        if (score[chance]+roll > 100)
+        if (score[chance]+roll > 100) //check if score + roll do not exceed 100
         {
-            return score;
+            return score; //return score without adding the roll dice value
         }
         else
         {
-            score[chance] += roll;
+            score[chance] += roll; 
         }
-            return score;
+            return score; //return score after adding the roll dice value
     }
 
     
@@ -59,13 +59,14 @@ class SnakeNLadder
         //taking input 
         Console.WriteLine("Enter the number of players");
         int numplayer = Convert.ToInt32(Console.ReadLine());
-        if(numplayer<2 || numplayer > 4)
+
+        if(numplayer<2 || numplayer > 4) //check if number of player is not less than 2 or greater than 4
         {
             Console.Error.WriteLine("the min player is 2 and max player is 4");
-            Environment.Exit(0);
+            Environment.Exit(0); //successfully terminate the code
         }
 
-        GameStart(numplayer);
+        GameStart(numplayer); // call method to start the game
     }
 
 
@@ -108,33 +109,37 @@ class SnakeNLadder
         int chance = 0;
 
 
-        while (true)
+        while (true) //loop run until one of the player win
         {
             Console.WriteLine(name[chance] + " press 1 to roll the dice ");
             int n = Convert.ToInt32(Console.ReadLine());
-            if (n == 1)
+
+            if (n == 1) //check if user input the valid value
             {
-                int roll = RandomDice();
+                int roll = RandomDice();  //call randomdice method to generate the random number and store in roll
+
                 Console.WriteLine("roll = " + roll);
-                int old = score[chance];
-                score = Move(chance, roll, score);
 
-                score = CheckSnake(snake, score, chance);
+                int old = score[chance]; //saving the old score of player in old
 
-                score = CheckLadder(ladder, score, chance);
+                score = Move(chance, roll, score); //call move method to update score accordingly
 
-                int new1 = score[chance];
+                score = CheckSnake(snake, score, chance); // method call for check for snake
 
-                Console.WriteLine(old + " -----> " + new1);
+                score = CheckLadder(ladder, score, chance);//method call for check for ladder
 
+                int new1 = score[chance];  //store the new score value of the player
 
-
-                Console.WriteLine(name[chance] + "'s score = " + score[chance]);
+                Console.WriteLine(old + " -----> " + new1); //print old and new score
 
 
 
+                Console.WriteLine(name[chance] + "'s score = " + score[chance]); //updated score
 
-                for (int i = 0; i < score.Length; i++)
+
+
+
+                for (int i = 0; i < score.Length; i++) // if any player won the code exit successfully
                 {
                     if (score[i] >= 100)
                     {
@@ -154,7 +159,7 @@ class SnakeNLadder
                 }
 
             }
-            else
+            else // if user input wrong 
             {
                 Console.WriteLine("please press 1 ");
             }
