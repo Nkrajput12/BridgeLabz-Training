3d99941dc2d83ed0489478e81b28fe186a022ce0
Nkrajput12 <nand.kishor_cs22@gla.ac.in>
2026-01-07 22:06:07 +0530

    [Nand kishor]Add: solved provlem

---
+++ oops-csharp-practice/gcr-codebase/csharp-Encapsulation-Polymorphism-Interface-Abstract/EmployeeManagement/Employee.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabzTraining.EmployeeManagement
+{
+    public abstract class Employee : IDepartment
+    {
+        private int Id;
+        private string Name;
+        private double BaseSalary;
+        private string DepartmentName;
+
+        public int id { get { return Id; } set { Id = value; } }
+        public string name { get { return Name; } set { Name = value; } }
+
+        public double baseSalary { get { return BaseSalary; } set { BaseSalary = value; } }
+
+        public Employee(int id, string name, double baseSalary)
+        {
+            this.id = id;
+            this.name = name;
+            BaseSalary = baseSalary;  
+        }
+
+        public abstract double CalculateSalary();
+
+        public void DisplayDetails()
+        {
+            Console.WriteLine("------------------------------------------------");
+            Console.WriteLine("Employee ID: " + Id);
+            Console.WriteLine("Name       : " + Name);
+            Console.WriteLine("Department : " + GetDepartmentName());
+            Console.WriteLine("Total Pay  : " + CalculateSalary().ToString());
+        }
+
+        public void AssignDepartment(string deptName)
+        {
+            DepartmentName = deptName;
+        }
+
+        public string GetDepartmentName()
+        {
+            if (DepartmentName == null)
+            {
+                return "Not Assigned";
+            }
+            return DepartmentName;
+        }
+}
+}
---
+++ oops-csharp-practice/gcr-codebase/csharp-Encapsulation-Polymorphism-Interface-Abstract/EmployeeManagement/PartTimeEmployee.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabzTraining.EmployeeManagement
+{
+    public class PartTimeEmployee : Employee
+    {
+        private double hourlyRate;
+        private int hoursWorked;
+
+        public double HourlyRate
+        {
+            get { return hourlyRate; }
+            set { hourlyRate = value; }
+        }
+
+        public int HoursWorked
+        {
+            get { return hoursWorked; }
+            set { hoursWorked = value; }
+        }
+
+        public PartTimeEmployee(int id, string name, double hourlyRate, int hoursWorked)
+            : base(id, name, 0)
+        {
+            hourlyRate = hourlyRate;
+            hoursWorked = hoursWorked;
+        }
+
+        public override double CalculateSalary()
+        {
+            return HourlyRate * HoursWorked;
+        }
+    }
+}
