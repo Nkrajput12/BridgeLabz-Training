45e1c4ac42d14ce38e6689e92cd32ef39e1c087f
Nkrajput12 <nand.kishor_cs22@gla.ac.in>
2026-01-07 22:54:07 +0530

    [Nand kishor] Add:Ecommerce platform

---
+++ oops-csharp-practice/gcr-codebase/csharp-Encapsulation-Polymorphism-Interface-Abstract/ECommercePlatform/Clothing.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabzTraining.ECommercePlatform
+{
+    public class Clothing : Product, ITaxable
+    {
+        public Clothing(int id, string name, double price) : base(id, name, price) { }
+
+        public override double CalculateDiscount()
+        {
+            
+            return 5.00;
+        }
+
+        public override double CalculateTax()
+        {
+            return Price * 0.05; 
+        }
+
+        public string TaxDetails()
+        {
+            return "Clothing  Tax  = 5%";
+        }
+    }
+}
---
+++ oops-csharp-practice/gcr-codebase/csharp-Encapsulation-Polymorphism-Interface-Abstract/ECommercePlatform/Product.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabzTraining.ECommercePlatform
+{
+    public abstract class Product
+    {
+        private int productId;
+        private string name;
+        private double price;
+
+        public int ProductId
+        {
+            get { return productId; }
+            set { productId = value; }
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        public double Price
+        {
+            get { return price; }
+            set { price = value; }
+        }
+
+        protected Product(int id, string name, double price)
+        {
+            this.productId = id;
+            this.name = name;
+            this.price = price;
+        }
+
+        // Abstract method for category-specific discounts
+        public abstract double CalculateDiscount();
+        public abstract double CalculateTax();
+    }
+}
