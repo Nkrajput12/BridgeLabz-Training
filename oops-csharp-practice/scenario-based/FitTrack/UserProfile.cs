cf9848992cb64466e303a04a35cebae5c080a3cd
Nkrajput12 <nand.kishor_cs22@gla.ac.in>
2026-01-09 12:16:12 +0530

    [Nand kishor] Add: scenario base Question name FitTrack

---
+++ oops-csharp-practice/scenario-based/FitTrack/UserProfile.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabzTraining.FitTrack
+{
+    public class UserProfile
+    {
+        private String Name { get; set; }
+        private int Age     { get; set; }
+        public double Height;
+        public double Weight;
+        public double TotalCalorieburn = 0;
+
+        public UserProfile(String name, int age, double weight, double height) //constructor to intiallize members
+        {
+            this.Name = name;
+            this.Age = age;
+            this.Height = height;
+            this.Weight = weight;
+        }
+
+        protected void Display() //method to displaay details
+        {
+            Console.WriteLine($"Name = {Name}\nAge = {Age}\nHeight = {Height}\nWeight = {Weight}\n Total calorie burn = {TotalCalorieburn}");
+        }
+    }
+}
