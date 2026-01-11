18c85a6a2b0c846cac1c2d80ff8ecea2a7ca3cda
Nkrajput12 <nand.kishor_cs22@gla.ac.in>
2026-01-11 14:36:30 +0530

    [Nand kishor] Add: sorting algorithms

---
+++ datastructure-csharp-practice/gcr-codebase/csharp-sorting-algorithms/InsertionSort.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+class InsertionSort
+{
+    static void Main(string[]args)
+    {
+        // Sample Employee IDs
+        int[] employeeIds = { 105, 101, 110, 103, 108, 102 };
+
+        Console.WriteLine("Ids before sorting");
+        foreach (int employeeId in employeeIds)
+        {
+            Console.WriteLine(employeeId);
+        }
+
+        InsertionSorting(employeeIds);
+
+        Console.WriteLine("Ids after sorting");
+        foreach(int employeeId in employeeIds)
+        {
+            Console.WriteLine(employeeId);
+        }
+    }
+
+    static void InsertionSorting(int[] array)
+    {
+        int n = array.Length;
+
+        // Start from the second element (index 1)
+        for (int i = 1; i < n; i++)
+        {
+            int key = array[i]; // The ID we want to insert
+            int j = i - 1;
+
+            
+            // to one position ahead of their current position
+            while (j >= 0 && array[j] > key)
+            {
+                array[j + 1] = array[j];
+                j = j - 1;
+            }
+
+            // Place the key into its correct spot
+            array[j + 1] = key;
+        }
+    }
+}
---
+++ datastructure-csharp-practice/gcr-codebase/csharp-sorting-algorithms/QuickSort.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabzTraining
+{
+
+    class QuickSort
+    {
+        static void Main(string[]args)
+        {
+            // Sample product prices
+            double[] productPrices = { 1200.50, 450.00, 89.99, 1500.00, 320.75, 99.99 };
+
+            Console.WriteLine("Original Prices Before Sorting");
+            foreach (double price in productPrices)
+            {
+                Console.WriteLine(price);
+            }
+
+            Quicksort(productPrices, 0, productPrices.Length - 1);
+
+            Console.WriteLine("Prices after  Sorting");
+            foreach (double price in productPrices)
+            {
+                Console.WriteLine(price);
+
+            }
+
+            static void Quicksort(double[] array, int low, int high)
+            {
+                if (low < high)
+                {
+                    // pi is the partitioning index, array[pi] is now at right place
+                    int pi = Partition(array, low, high);
+
+                    // Recursively sort elements before and after partition
+                    Quicksort(array, low, pi - 1);
+                    Quicksort(array, pi + 1, high);
+                }
+            }
+
+            static int Partition(double[] array, int low, int high)
+            {
+                // Choosing the last element as the pivot
+                double pivot = array[high];
+                int i = (low - 1); // Index of smaller element
+
+                for (int j = low; j < high; j++)
+                {
+                    // If current element is smaller than the pivot
+                    if (array[j] < pivot)
+                    {
+                        i++;
+                        // Swap array[i] and array[j]
+                        double temp = array[i];
+                        array[i] = array[j];
+                        array[j] = temp;
+                    }
+                }
+
+                // Swap the pivot element with the element at i + 1
+                double tempPivot = array[i + 1];
+                array[i + 1] = array[high];
+                array[high] = tempPivot;
+
+                return i + 1;
+            }
+        }
+    }
+}
