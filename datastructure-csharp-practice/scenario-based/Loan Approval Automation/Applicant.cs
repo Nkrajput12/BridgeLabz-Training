9ba2f25c0c689ace07bcaa1b91fbf2c55a45dc80
Nkrajput12 <nand.kishor_cs22@gla.ac.in>
2026-01-12 19:17:27 +0530

    [Nand kishor] Add: 2 Scenario base problems

---
+++ datastructure-csharp-practice/scenario-based/Loan Approval Automation/Applicant.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabzTraining.Loan_Approval_Automation
+{
+    public class Applicant
+    {
+        private string Name;
+        protected int CreditScore;
+        protected double income;
+        protected double LoanAmount;
+
+        public Applicant(string name , int creditScore, double income, double loanAmount)
+        {
+            Name = name;
+            CreditScore = creditScore;
+            this.income = income;
+            LoanAmount = loanAmount;
+        }
+
+        public void Display()
+        {
+            Console.WriteLine("-----------Costumer Details-----------");
+            Console.WriteLine("Name = " + Name);
+            Console.WriteLine("CreditScore = "+CreditScore);
+            Console.WriteLine("Income = "+income);
+            Console.WriteLine("Loan Amount" + LoanAmount);
+        }
+    }
+}
---
+++ datastructure-csharp-practice/scenario-based/Loan Approval Automation/LoanApprovaHome.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabzTraining.Loan_Approval_Automation
+{
+    public class LoanApprovaHome
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Enter Name: ");
+            string name = Console.ReadLine();
+            Console.Write("Enter Credit Score: ");
+            int creditScore = int.Parse(Console.ReadLine());
+            Console.Write("Enter Income: ");
+            double income = double.Parse(Console.ReadLine());
+            Console.Write("Enter Loan Amount: ");
+            double loanAmount = double.Parse(Console.ReadLine());
+            Console.Write("Enter the Loan Term: ");
+            int term = int.Parse(Console.ReadLine());
+
+            LoanApprovalMenu menu = new LoanApprovalMenu();
+            menu.Run(name, creditScore,income,loanAmount,term);
+        }
+    }
+}
