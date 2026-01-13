8f4e5abd42e10cd61152f5a4abb21eaa14d5e138
Nkrajput12 <nand.kishor_cs22@gla.ac.in>
2026-01-13 16:15:02 +0530

    [Nand kishor] Add: scenerio base question movie schedule manager

---
+++ datastructure-csharp-practice/scenario-based/Movie_Schedule_Manager/InvalidTimeFormatException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabzTraining.Movie_Schedule_Manager
+{
+    internal class InvalidTimeFormatException : Exception
+    {
+        public InvalidTimeFormatException(string Message) : base(Message) { }
+    }
+}
---
+++ datastructure-csharp-practice/scenario-based/Movie_Schedule_Manager/MoviesMenu.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabzTraining.Movie_Schedule_Manager
+{
+    internal class MoviesMenu
+    {
+        MovieUtility utility = new MovieUtility();
+        
+        public void RunMenu()
+        {
+            bool exit = false;
+
+            while (!exit)
+            {
+                Console.WriteLine("\n--- Cinema Time Manager ---");
+                Console.WriteLine("Press 1. Add Movie");
+                Console.WriteLine("Press 2. View All Movies");
+                Console.WriteLine("Press 3. Search Movie");
+                Console.WriteLine("Press 4. Exit");
+                Console.Write("Select an option: ");
+                int choice = int.Parse(Console.ReadLine());
+
+                try
+                {
+                    switch (choice)
+                    {
+                        case 1: utility.AddMovie(); break;
+                        case 2: utility.DisplayAll(); break;
+                        case 3: utility.SearchMovies(); break;
+                        case 4: exit = true; break;
+                        default: Console.WriteLine("Invalid choice."); break;
+                    }
+                }
+                catch(IndexOutOfRangeException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                catch(InvalidTimeFormatException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Unexpected Error: {ex.Message}");
+                }
+            }
+        }
+    }
+}
