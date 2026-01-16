ec39c090a78cc70a89c3a45037b5ffd167fc7a4b
Nkrajput12 <nand.kishor_cs22@gla.ac.in>
2026-01-16 12:41:34 +0530

    [Nand kishor] Add: Scenario basedquestion browser buddy

---
+++ datastructure-csharp-practice/scenario-based/BrowserBuddy/ITab.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabzTraining.BrowserBuddy
+{
+    public interface ITab
+    {
+        void Push(string data);
+
+        string Pop();
+    }
+}
