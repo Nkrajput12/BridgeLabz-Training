e162abe8c0b73e74d1763da0c586fbbf32deadde
Nkrajput12 <nand.kishor_cs22@gla.ac.in>
2026-01-25 21:28:38 +0530

    [Nand kishor]Add: Fitness TrackerScenario based

---
+++ datastructure-csharp-practice/scenario-based/FitnessTracker/Customer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabzTraining.FitnessTracker
+{
+    public  class Customer
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public int StepCount { get; set; }
+
+        public Customer(string name, int age)
+        {
+            Name = name;
+            Age = age;
+            StepCount = 0;
+        }
+
+    }
+}
---
+++ datastructure-csharp-practice/scenario-based/FitnessTracker/FitnessTracker.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabzTraining.FitnessTracker
+{
+    internal class FitnessTracker
+    {
+        public static void Main(string[] args)
+        {
+            FitnessMenu menu = new FitnessMenu();
+            menu.Run();
+        }
+    }
+}
