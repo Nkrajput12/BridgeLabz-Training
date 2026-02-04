dcf57d6d3ad62e9cc106ea41d07b6afad26096ae
Nkrajput12 <nand.kishor_cs22@gla.ac.in>
2026-02-04 16:07:02 +0530

    [Nand kishor] Add: Robot hazard analyzer scenario base problem

---
+++ collection-csharp-practice/scenario-based/factory_robot_hazard_analyzer/RobotSafetyException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabzTraining.factory_robot_hazard_analyzer
+{
+    internal class RobotSafetyException : Exception
+    {
+        public RobotSafetyException(string message): base(message) { }
+
+    }
+}
